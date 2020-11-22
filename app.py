cd195f8e4e143169ec36e1c43c8d90d4863817f5
Raindow00S <47203444+Raindow00S@users.noreply.github.com>
2020-11-22 21:11:33 +0800

    显示个人信息

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ import json
 import util
 import db
 
+# 【试图设置全局变量，以在整个登录过程中保存信息方便查找】
+# userID = "0" # 账号（学生-学号；教师-职工号；仪器管理员-职工号）
+# identity = "undefined"  # 身份（学生/教师/仪器管理员）
+userID = "01"   # 学生测试
+identity = "student"
+# userID = "02"   # 老师测试
+# identity = "faculty"
+# userID = "03"   # 仪器管理员测试
+# identity = "equipment_manager"
+
 app = Flask(__name__) #首先定义一个应用程序 Flask构造函数使用当前模块的名称作为参数
 # 路由配置
 # @app.route('/')
@@ -61,10 +71,27 @@ def login():
         else:   # 密码存在且符合
             return 'right'
 
-@app.route('/user', methods=['POST'])
+@app.route('/user', methods=['POST'])   # 只有服务器接收到信息，才会跳转至/user
 def login_success():
     username = request.form.get('username', default='user')
     return render_template('user.html', username=username), 200     # 将用户名传入html进行渲染
+
+@app.route('/myinfo', methods=['GET','POST'])
+def showInfo():
+    logger.info("全局变量userID："+userID)
+    logger.info("全局变量identity："+identity)
+    # 根据身份和账号(全局变量)，从数据库取出和身份对应的个人信息，传给前端
+    # 若数据库正常，不会出现找不着的情况
+    db_info = db.get_info(userID, identity) # 返回的是个字典
+    logger.info("获得的信息："+str(db_info))
+    
+    # 传送数据给前端
+    # 【老冯，拆字典的任务就交给你了！】
+    return render_template('myinfo.html',
+                            identity=identity,
+                            info=db_info)
+
+
 
 if __name__ == '__main__':
     host = util.get_config()["host"]    # 获取配置信息
--- db.py
+++ db.py
@@ -50,6 +50,40 @@ def get_pass(username):
     else:   # 若结果不为空，返回第一条
         return password[0][0]
 
+# 【TO DO 根据身份和账号(全局变量)，从数据库取出和身份对应的个人信息，传给前端】
+def get_info(userID, identity):
+    try:
+        conn = get_connect()    # 建立连接和游标
+        cursor = conn.cursor()
+        # 通过身份判断查询哪个关系表
+        if identity == 'student':
+            print("身份校对：student")
+            cursor.execute('select * from student where 学号 = %s', (userID,))
+        elif identity == 'faculty':
+            print("身份校对：faculty")
+            cursor.execute('select * from faculty where 职工号 = %s', (userID,))
+        else:
+            print("身份校对：equipment_manager")
+            cursor.execute('select * from equipment_manager where 职工号 = %s', (userID,))
+        info = cursor.fetchone()    # 正常情况下，会查询出一条数据，只取这一条即可（数据类型为元组）
+    except Exception as e:  # 报错
+        print(e)
+    finally:
+        cursor.close()  # 关闭游标和连接
+        conn.close()
+    
+    # 将元组中的数据和属性名拼接成字典 属性名:属性值
+    if identity == 'student':
+        attr = ["学号", "姓名", "院系", "专业", "年级"]
+    elif identity == 'faculty':
+        attr = ["职工号", "姓名", "职称"]
+    else:
+        attr = ["职工号", "姓名"]
+
+    info_dict = dict(zip(attr, info))
+    # 返回字典
+    return info_dict
+
 if __name__ == '__main__':
     save_user("xhh", "123")
     print(get_pass("xhh"))
